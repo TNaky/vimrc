e4663571c524e3864cedc42eb16fb549ecba608c
TNak@MacMini <michibata.dev@gmail.com>
2016-02-25 13:45:49 +0900

    OSを判定する処理を追記（判定後はVimのインストールを書く予定）

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+# OSを判定
+OS=''
+if [ `uname` = 'Darwin' ]; then
+  OS='Mac'
+elif [ `uname` = 'Linux' ]; then
+  if [ `cat /etc/*-release | grep Ubuntu 2>&1 /dev/null; echo $?` ]; then
+    OS='Ubuntu'
+  elif [ `cat /etc/*-release | grep Debian 2>&1 /dev/null; echo $?` ]; then
+    OS='Debian'
+  fi
+fi
+echo $OS
+
+# vimrcをgithubからクローンする
 mkdir -p ${HOME}/.vim
 git clone https://github.com/TNaky/vimrc.git ${HOME}/.vim/vimrc
 ln -s ${HOME}/.vim/vimrc/.vimrc ${HOME}/.vimrc
